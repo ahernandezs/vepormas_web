53762883e0201fe200c181b11641110817def9de
Jose Castaneyra <jcastaneyra@gmail.com>
2014-02-17 23:37:58 -0600

    ABNK-424 Some cleaning done

--- SPA/app/scripts/controllers/auth.js
+++ SPA/app/scripts/controllers/auth.js
@@ -5,54 +5,54 @@
  * inject a login function in the scope
  */
 angular.module('spaApp')
-  .controller('LoginCtrl', function ($scope,$http,$location, $cookieStore, api) {
-	/**
-	 * the login function connect the Rest-API: if the response status is OK, redirect to route "accounts",
-	 * else put an error message in the scope
-	 */
-	$scope.login=function(){
-		$http({
-			url: 'http://abanking-ext-api.herokuapp.com/api/login',
-			method: 'POST',
-			data: JSON.stringify({'username':$scope.username, 'password':$scope.password,'access_media': 'SPA'})
-		}).
-		success(function(data, status, headers) {
-			//get the session token from the response and store it in a cookieStore
-			var token = headers('X-AUTH-TOKEN');
+.controller('LoginCtrl', function ($scope,$http,$location, $cookieStore, api) {
+  /**
+   * the login function connect the Rest-API: if the response status is OK, redirect to route "accounts",
+   * else put an error message in the scope
+   */
+  $scope.login=function(){
+    $http({
+      url: 'http://abanking-ext-api.herokuapp.com/api/login',
+      method: 'POST',
+      data: JSON.stringify({'username':$scope.username, 'password':$scope.password,'access_media': 'SPA'})
+    }).
+      success(function(data, status, headers) {
+      //get the session token from the response and store it in a cookieStore
+      var token = headers('X-AUTH-TOKEN');
 
-			api.init(token);
+      api.init(token);
 
-			$cookieStore.put('token', token);
+      $cookieStore.put('token', token);
 
-			$location.path( '/accounts' );
-		}).
-        error(function(data, status) {
-			//put an error message in the scope
-			$scope.errorMessage = 'login failed';
-			$scope.status = status;
-		});
-	};
+      $location.path( '/accounts' );
+    }).
+      error(function(data, status) {
+      //put an error message in the scope
+      $scope.errorMessage = 'login failed';
+      $scope.status = status;
+    });
+  };
 
-	$scope.logout = function() {
-		$http({
-			url: 'http://abanking-ext-api.herokuapp.com/api/logout',
-			method: 'GET'
-		}).
-		success(function(data, status, headers) {
-			// removes token in the cookieStore
-			$scope.message = 'logout successful';
-			$scope.status = status;
-			$cookieStore.remove('token');
-			$location.path( '/login' );
-		}).
-        error(function(data, status) {
-			//put an error message in the scope
-			$scope.errorMessage = 'logout failed';
-			$scope.status = status;
-			$cookieStore.remove('token');
-			$location.path( '/login' );
-		});
-	}
+  $scope.logout = function() {
+    $http({
+      url: 'http://abanking-ext-api.herokuapp.com/api/logout',
+      method: 'GET'
+    }).
+      success(function(data, status, headers) {
+      // removes token in the cookieStore
+      $scope.message = 'logout successful';
+      $scope.status = status;
+      $cookieStore.remove('token');
+      $location.path( '/login' );
+    }).
+      error(function(data, status) {
+      //put an error message in the scope
+      $scope.errorMessage = 'logout failed';
+      $scope.status = status;
+      $cookieStore.remove('token');
+      $location.path( '/login' );
+    });
+  }
 });
 
 
