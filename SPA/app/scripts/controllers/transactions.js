0df2364303049c11bed9372cbc37a586acdddff4
Jose Castaneyra <jcastaneyra@gmail.com>
2014-02-18 16:36:57 -0600

    ABNK-418 Added partials to show account and transaction details

--- SPA/app/scripts/controllers/transactions.js
+++ SPA/app/scripts/controllers/transactions.js
@@ -13,4 +13,20 @@ angular.module('spaApp').controller('TransactionsCtrl', function($scope, $http, 
     console.log(data, status);
     $location.path( '/login' );
   });
+
+  $scope.showAccount = true;
+  $scope.showTxnDetail = false;
+  $scope.currentTransaction = undefined;
+
+  $scope.showTransactionDetail = function(transaction) {
+    if($scope.currentTransaction && $scope.currentTransaction._transaction_id === transaction._transaction_id) {
+      $scope.showAccount = true;
+      $scope.showTxnDetail = false;
+      $scope.currentTransaction = undefined;
+    } else {
+      $scope.showAccount = false;
+      $scope.showTxnDetail = true;
+      $scope.currentTransaction = transaction;
+    }
+  }
 });
