612f4deeaf912d1a513c0e707ef72ff92a56ae65
Jose Castaneyra <jcastaneyra@gmail.com>
2014-02-20 13:13:36 -0600

    ABNK-420 Added service payment simulation

--- SPA/app/scripts/controllers/transactions.js
+++ SPA/app/scripts/controllers/transactions.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('spaApp').controller('TransactionsCtrl', function($scope, $http, $location, $routeParams) {
+angular.module('spaApp').controller('TransactionsCtrl', function($scope, $http, $location, $routeParams, $timeout) {
   $http({
     url: $scope.restAPIBaseUrl+'/accounts/' + $routeParams.accountId + '/transactions',
     method: 'GET'
@@ -14,19 +14,43 @@ angular.module('spaApp').controller('TransactionsCtrl', function($scope, $http, 
     $location.path( '/login' );
   });
 
-  $scope.showAccount = true;
-  $scope.showTxnDetail = false;
+  $scope.selection = "";
   $scope.currentTransaction = undefined;
 
   $scope.showTransactionDetail = function(transaction) {
     if($scope.currentTransaction && $scope.currentTransaction._transaction_id === transaction._transaction_id) {
-      $scope.showAccount = true;
-      $scope.showTxnDetail = false;
+      $scope.selection = undefined;
       $scope.currentTransaction = undefined;
     } else {
-      $scope.showAccount = false;
-      $scope.showTxnDetail = true;
+      $scope.selection = 'transaction';
       $scope.currentTransaction = transaction;
     }
+  }
+
+  $scope.close = function() {
+    $scope.selection = undefined;
+    $scope.currentTransaction = undefined;
+  }
+
+  $scope.showServices = function() {
+    $scope.selection = "services";
+    $scope.currentTransaction = undefined;
+  }
+
+  $scope.showServicePayment = function() {
+    $scope.selection = 'servicespayment';
+  }
+
+  $scope.showServicePaymentToken = function() {
+    $scope.selection = 'servicespaymenttoken';
+    $scope.token = undefined;
+
+    $timeout( function() {
+      $scope.token = true;
+    },2000);
+  }
+
+  $scope.applyServicePayment = function() {
+    $scope.selection = 'applyservicespayment';
   }
 });
