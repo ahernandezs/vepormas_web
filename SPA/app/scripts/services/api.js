0bf06698640e5f4d4af2767f86a7e105f3e6e206
Jose Castaneyra <jcastaneyra@gmail.com>
2014-03-11 18:21:57 -0600

    ABNK-427 spa.properties was removed, creates overload to page loading

--- SPA/app/scripts/services/api.js
+++ SPA/app/scripts/services/api.js
@@ -4,20 +4,20 @@
  * api initializer factory
  */
 
-angular.module('spaApp').factory('api', function ($http, $cookieStore,$rootScope) {
+angular.module('spaApp').factory('api', function ($http, $cookieStore, $rootScope) {
   var hasBeenConfigured = false;
-  return {  	
+  return {
     init: function (token) {
       // this is the token of the bank
-      $http.defaults.headers.common['X-BANK-TOKEN'] = 1;
+      $http.defaults.headers.common['X-BANK-TOKEN'] = 3;
       $http.defaults.headers.common['X-AUTH-TOKEN'] = token || $cookieStore.get('token');
+
+      console.log("Executes init & token = " + $cookieStore.get('token'));
     },
     config: function(){
-    	$http.get('spa.properties').then(function (response) {
-        	  $rootScope.restAPIBaseUrl = response.data.restAPBaseIUrl;
-            $rootScope.subscribeKey = response.data.subscribeKey;
-            $rootScope.publishKey = response.data.publishKey;
-        });     	
+      $rootScope.restAPIBaseUrl = "http://projects.anzen.com.mx:8081/Abanking-Core/rest";
+      $rootScope.subscribeKey = "sub-c-74d472c2-894a-11e3-a56b-02ee2ddab7fe";
+      $rootScope.publishKey = "pub-c-d49c709c-8d0c-40ed-aaa6-66cbc20683d0";
     }
   };
 });
