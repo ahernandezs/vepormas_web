3f489369d2747e32ded8c3d177a404f549034d66
Armando Máximo Hernández <ahernandezs@anzen.com.mx>
2014-07-08 17:53:49 -0500

    More functions in Third Accounts
    Third account services connected with the RESTApi
    
    Fixed link to RestApi

--- SPA/app/scripts/services/third-account-service.js
+++ SPA/app/scripts/services/third-account-service.js
@@ -1,16 +1,21 @@
 'use strict';
 
 angular.module('spaApp')
-  .service('thirdAccountService', ['$http',function ($http) {
+  .service('thirdAccountService', ['$http','$rootScope',function ($http, $rootScope) {
 	this.getThirdAcounts = function(){
-		//return $http.get($rootScope.restAPIBaseUrl+'/externalaccounts');
-		return $http.get('/scripts/services/third-account-beneficiaries-JSON.js');	
+		return $http({
+				url: $rootScope.restAPIBaseUrl+'/externalaccounts',
+				//url: '/scripts/services/third-account-beneficiaries-JSON.js',
+				method: 'GET',
+				headers: {'Content-Type': 'application/json','X-AUTH-TOKEN': '1'}
+		});
+
+
 	},
 
 	this.setBeneficiary = function(name, clabe, amount, email, phone){
 		return $http({
-				//url: $rootScope.restAPIBaseUrl+'/externalaccounts',
-				url:'/scripts/services/third-account-beneficiaries-JSON.js',
+				url: $rootScope.restAPIBaseUrl+'/externalaccounts',
 				method: 'POST',
 				data: JSON.stringify({
 					'name':name,
@@ -19,7 +24,23 @@ angular.module('spaApp')
 					'email':email,
 					'phone':phone
 				}),
-				headers: {'Content-Type': 'application/json','X-BANK-TOKEN': '1'}
+				headers: {'Content-Type': 'application/json','X-AUTH-TOKEN': '1'}
+		});
+	},
+
+	this.deleteThirdAccount = function(thirdAccountID){
+		return $http({
+				url: $rootScope.restAPIBaseUrl+'/'+thirdAccountID,
+				method: 'DELETE',
+				headers: {'Content-Type': 'application/json','X-AUTH-TOKEN': '1'}
+		});
+	}
+
+	this.deleteThirdAccount = function(thirdAccountID){
+		return $http({
+				url: $rootScope.restAPIBaseUrl+'/'+thirdAccountID,
+				method: 'PUT',
+				headers: {'Content-Type': 'application/json','X-AUTH-TOKEN': '1'}
 		});
 	}
 }]);
