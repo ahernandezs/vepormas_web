b8b566762a7d575197ce39a083e3bd5e0ea891d6
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-11-28 13:33:33 -0600

    ABNK-655 Implements login controller

--- SPA/app/scripts/controllers/auth.js
+++ SPA/app/scripts/controllers/auth.js
@@ -14,34 +14,38 @@ angular.module('spaApp')
     $location.path('/accounts');
   }
 
-  $scope.buttonStatus = function(text, status) {
-    $scope.loginButtonStatus = {
-      text: text,
-      disabled: status
-    }
-  };
 
-  $scope.buttonStatus("Entrar", false);
+  $scope.checkUser = function(){
+    $http({
+      url: $scope.restAPIBaseUrl+'/checkLogin',
+      method: 'POST',
+      data: JSON.stringify({'user_login':$scope.username,'client_application_id': 'PROSA-DIG'})
+    }).
+      success(function(data, status, headers) {
+        console.log(data);
+    }).
+      error(function(data, status) {
+      $scope.errorMessage = data.message;
+      $scope.status = status;
+    });
+  }
 
-  $scope.login=function(){
-    $scope.buttonStatus("Entrando ...", true);
-
+  /**
+    Function for authenticate
+  **/
+  $scope.login = function(){
     $http({
       url: $scope.restAPIBaseUrl+'/login',
       method: 'POST',
-      data: JSON.stringify({'username':$scope.username, 'password':$scope.password,'access_media': 'SPA'})
+      data: JSON.stringify({'user_login':$scope.username, 'password':$scope.password,'client_application_id': 'PROSA-DIG'})
     }).
       success(function(data, status, headers) {
-      //get the session token from the response and store it in rootScope
       var token = headers('X-AUTH-TOKEN');
-
       $rootScope.session_token = token;
       $rootScope.last_access_date = data.last_access_date;
       $rootScope.last_access_media = data.last_access_media;
 
       api.init();
-
-      $scope.initPubNub();
 
       $location.path( '/accounts' );
     }).
@@ -49,32 +53,14 @@ angular.module('spaApp')
       //put an error message in the scope
       $scope.errorMessage = data.message;
       $scope.status = status;
-      $scope.buttonStatus("Entrar", false);
     });
-
 
   };
 
   $scope.logout = function() {
-    $http({
-      url: $scope.restAPIBaseUrl+'/logout',
-      method: 'GET'
-    }).
-      success(function(data, status, headers) {
-      // removes token in the rootScope
-      $scope.message = 'logout successful';
-      $scope.status = status;
-      $rootScope.session_token = null;
-      $location.path( '/login' );
-    }).
-      error(function(data, status) {
-      //put an error message in the scope
-      $scope.errorMessage = 'logout failed';
-      $scope.status = status;
-      $rootScope.session_token = null;
-      $location.path( '/login' );
-    });
   }
+
+
 }]);
 
 
