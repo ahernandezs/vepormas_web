24778390d3a9931095f802f79a32348b30341841
rvacoder <rvacoder@gmail.com>
2014-12-02 16:59:07 -0600

    Product partials created

---
+++ SPA/app/scripts/controllers/products.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * products controller
+ * 
+ */
+angular.module('spaApp')
+.controller('ProductsCtrl', ['$scope', '$http', '$location', 'api', '$rootScope', 'userProvider', function ($scope,$http,$location, api, $rootScope, userProvider) {
+  
+}]);
+
+
