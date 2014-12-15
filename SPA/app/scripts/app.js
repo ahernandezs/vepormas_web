af48e858893607f81db753ab29dc4bce7e2e0fec
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-12-15 12:20:31 -0600

    ABNK-662 Add changues for ui-router .

--- SPA/app/scripts/app.js
+++ SPA/app/scripts/app.js
@@ -6,64 +6,39 @@ var app = angular.module('spaApp', [
   'ngSanitize',
   'ngRoute',
   'infinite-scroll',
-  'ngTable'
+  'ngTable',
+  'ui.router',
+  'infinite-scroll'
 ]);
 
-app.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
-    //$httpProvider.responseInterceptors.push('httpInterceptor');
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
 
-    $routeProvider
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .when('/accounts', {
-        templateUrl: 'views/accounts.html',
-        controller: 'AccountsCtrl',
-        /*resolve: {
-          accounts: ['accountsProvider', function(accountsProvider) {
-            return accountsProvider.getAccounts();
-          }]*/
-      })
-      .when('/investments', {
-        templateUrl: 'views/investments.html',
-        controller: 'InvestmentsCtrl',
-      })
-      .when('/register',{
-        templateUrl: 'views/register.html',
-        controller: 'RegisterCtrl'
-      })
-      .when('/products',{
-        templateUrl: 'views/products.html',
-        controller: 'ProductsCtrl'
-      })
-      .otherwise({
-        redirectTo: '/login'
-      });
+    //$httpProvider.responseInterceptors.push('httpInterceptor');
+  $urlRouterProvider.otherwise("/login");
+  $stateProvider
+    .state('login', {
+    url: '/login',
+    templateUrl: 'views/login.html',
+    controller: 'LoginCtrl'
+    })
+
+    .state('accounts', {
+    url: '/accounts',
+    templateUrl: 'views/accounts.html',
+    controller: 'AccountsCtrl'
+    })
+
+    .state('register', {
+    url: '/register',
+    templateUrl: 'views/register.html',
+    controller: 'RegisterCtrl'
+    })
   }]);
 
 app.run(['api', '$window', '$rootScope',function(api, $window, $rootScope) {
   api.config();
   api.init();
 
-  //Escape string to be able to use it in a regular expression
- /* function regEsc(str) {
-      return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-  }
-  //When backend receives a request to the views folder, pass it through
-  $httpBackend.whenGET( RegExp( regEsc( 'views/' ) ) ).passThrough();
-  //Only load mocks if config says so
-  if(!$rootScope.useMocks) return;
-
-  $httpBackend.whenPOST($rootScope.restAPIBaseUrl + '/checkLogin').respond(function(method, url, data, headers) {
-    return [200, {}, {}];
-  });
-
-  $httpBackend.whenPOST($rootScope.restAPIBaseUrl + '/login').respond(function(method, url, data, headers) {
-    var message = {res:'mock'}
-    return [200, {}, {}];
-  });
- */
   $window.onbeforeunload = function(e) {
     var message = 'Te vas a salir de Consubanco, ¿est&aacute;s seguro?';
     e = e || $window.event;
