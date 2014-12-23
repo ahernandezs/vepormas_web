aaa9ce62d29dd5d7d959a2750059c9c885d3ea4a
Armando Máximo Hernández <ahernandezs@anzen.com.mx>
2014-12-23 11:58:50 -0600

    investment detail

--- SPA/app/scripts/controllers/investments.js
+++ SPA/app/scripts/controllers/investments.js
@@ -3,26 +3,13 @@
 /**
  * The accounts controller. Gets accounts passing auth parameters
  */
-angular.module('spaApp').controller('InvestmentsCtrl', ['$scope', '$location', 'ngTableParams', function ($scope, $location, ngTableParams) {
-  //TODO: temporal binding
-  $scope.completeName = 'ABEL BECERRA CASTRO';
-  $scope.date = '17/05/2014';
-  $scope.time = '23:34:54';
+angular.module('spaApp').controller('InvestmentsCtrl', ['$scope', '$location', 'ngTableParams',  '$stateParams', 'accountsProvider', '$rootScope', function ($scope, $location, ngTableParams, $stateParams, accountsProvider, $rootScope) {
 
-  $scope.showAccountHeader = true;
-
-  $scope.accountHeader = {'cutDate':'29/Oct/2014',
-              'minimumPayment':'760.00',
-              'totalPayment':'2,123.00',
-              'limitDate':'15/Oct/2014',
-              'limitPayment':'760.00',
-              'cutBalance':'2,123.00',
-              'currentBalance':'2,123.00',
-              'noInterest':'1,520.00',
-              'dueCurrent':'0.00',
-              'limitCredit':'5,000.00',
-              'availableCredit':'3,000.00'};
-
+    accountsProvider.getAccountDetail($stateParams.accountId+'-INV').then(
+      function(data) {
+        $scope.investmentHeader = $rootScope.accounts.investment;
+      }
+    );
 
     var data = [{date: "Sep 18", transactionNumber: "001", description: "La corbata", operationType: "Pago", amount: "3000"},
                 {date: "Ago 19", transactionNumber: "002", description: "Solid Gold", operationType: "Pago", amount: "5400"}];
@@ -38,12 +25,5 @@ angular.module('spaApp').controller('InvestmentsCtrl', ['$scope', '$location', '
         }
     });
 
-
-  /**
-    Function for logout application
-  **/
-  $scope.logout = function() {
-  $location.path('/login');
-  }
 
 }]);
