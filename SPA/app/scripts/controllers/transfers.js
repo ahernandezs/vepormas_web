9c7eef977f9732abab479d24c6b611f2afb9e45f
Jesus Macedo <macedo.rjpablo@gmail.com>
2015-01-13 17:27:34 -0600

    Created object to send for transfer. Also added data binding for partials.

--- SPA/app/scripts/controllers/transfers.js
+++ SPA/app/scripts/controllers/transfers.js
@@ -6,6 +6,12 @@
 angular.module('spaApp').controller('TransfersCtrl', ['$rootScope', '$scope', '$location', '$routeParams', 'accountsProvider', 'userProvider', function ($rootScope, $scope, $location, $routeParams, accountsProvider, userProvider) {
 	
     $scope.selection = 1;
+    $scope.transfer = {};
+    $scope.transfer.account;
+    $scope.transfer.destination;
+    $scope.transfer.amount;
+    $scope.transfer.concept;
+    $scope.transfer.date = 'today';
     
 
 	accountsProvider.getAccounts().then(
