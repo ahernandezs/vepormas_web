74adfaa74e24e85eaa88dae4b37415cb21dd4137
Armando Máximo Hernández <ahernandezs@anzen.com.mx>
2015-01-23 19:01:03 -0600

    first commit

--- SPA/app/scripts/controllers/admin.js
+++ SPA/app/scripts/controllers/admin.js
@@ -3,12 +3,22 @@
 angular.module('spaApp').controller('adminCtrl', ['$rootScope', '$scope', 'adminProvider', function ($rootScope, $scope, adminProvider) {
 
 	$scope.selection = 1;
+	$scope.action = 1;
 
-	$scope.selectBeneficiary = function(){
-		$scope.action = 1;
-		console.log('mostrar la partial con el usuario, el beneficiary-delete-1.html');
+	$scope.selectBeneficiary = function(account){
+		$scope.action = 2;
+		$scope.stage = 1;
+		$scope.selectedAccount = account;
 	}
 
+	$scope.siguiente = function(){
+		$scope.stage += 1;
+	}
+
+	$scope.regresar = function(){
+		$scope.action = 1;
+		$scope.stage = 1;
+	}
 
 	adminProvider.getThirdAccounts().then(function(data) {
 		$scope.third_accounts = $rootScope.third_accounts;
