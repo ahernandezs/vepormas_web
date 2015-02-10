8ae3a04c0c4322ce4e0e6d1329b708bc3b7ac3a0
Armando Máximo Hernández <ahernandezs@anzen.com.mx>
2015-02-10 11:54:48 -0600

    geocoder

--- SPA/app/scripts/controllers/map.js
+++ SPA/app/scripts/controllers/map.js
@@ -3,6 +3,7 @@
 angular.module('spaApp').controller('MapCtrl', ['$scope', '$rootScope', 'mapProvider', function ($scope,  $rootScope, mapProvider) {
 
 	$scope.map = { center: { latitude: 19.3888201, longitude: -99.1930411 }, zoom: 15 };
+	var geocoder = new google.maps.Geocoder();
 
 	mapProvider.getMarkers({}).then(
 		function(data) {
@@ -29,6 +30,21 @@ angular.module('spaApp').controller('MapCtrl', ['$scope', '$rootScope', 'mapProv
 		} else {
 			var content = 'Error: Your browser doesn\'t support geolocation.';
 		}
+	}
+
+	function codeAddress(address) {
+	  geocoder.geocode( { 'address': address}, function(results, status) {
+	    if (status == google.maps.GeocoderStatus.OK) {
+	      //map.setCenter(results[0].geometry.location);
+	      /*var marker = new google.maps.Marker({
+	          map: map,
+	          position: results[0].geometry.location
+	      });*/
+			console.log('Resultado de la busqueda: '+JSON.stringify(results));
+	    } else {
+	      alert('Geocode was not successful for the following reason: ' + status);
+	    }
+	  });
 	}
 
 }]);
