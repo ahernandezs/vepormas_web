02db18afbf07afa442f6bf5920b4991ac4d1c751
Jesus Macedo <macedo.rjpablo@gmail.com>
2015-02-16 13:39:12 -0600

    Almost finished changes on UI.

--- SPA/app/scripts/controllers/portfolio.js
+++ SPA/app/scripts/controllers/portfolio.js
@@ -3,19 +3,45 @@
 /**
  * The transactions controller. For transactions between own accounts.
  */
-angular.module('spaApp').controller('PortfolioCtrl', ['$rootScope', '$scope', '$location', '$routeParams', 'accountsProvider', function ($rootScope, $scope, $location, $routeParams, accountsProvider) {
-	
+angular.module('spaApp').controller('PortfolioCtrl', ['$rootScope', '$scope', '$location', '$routeParams', 'accountsProvider', 'transferProvider', function ($rootScope, $scope, $location, $routeParams, accountsProvider, transferProvider) {
+
     $scope.selection = 1;
     $scope.portfolio = {};
     $scope.invType = 'VISTA';
-    
+    $scope.theAccounts = [];
+
+    /**
+     * Get investments products.
+     */
+    transferProvider.getProducts().then(
+        function(data) {
+            $scope.invProducts = $rootScope.invProducts;
+        }
+    );
+
+    /**
+     * Get the own accounts.
+     */
+    accountsProvider.getAccounts().then(
+        function(data) {
+            $rootScope.accounts.forEach(
+                function (value, index, ar) {
+                    value.group = 'Cuentas Propias';
+                    $scope.theAccounts.push( value );
+                }
+            );
+        }
+    );
+
     /**
      * Function to navigate between steps.
 	 */
-	 $scope.completeStep = function(nextStep) {
-         $scope.selection = nextStep;
+	$scope.completeStep = function(nextStep) {
+        $scope.selection = nextStep;
+		if (nextStep == 1)
+			$scope.portfolio = {};
 	 };
-    
+
     /**
      * Assigns the value to the first view to return to and the investment type.
      */
@@ -23,5 +49,5 @@ angular.module('spaApp').controller('PortfolioCtrl', ['$rootScope', '$scope', '$
         $scope.selection = 1;
         $scope.invType = type;
     };
-    
+
 }]);
