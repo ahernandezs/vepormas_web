83f78110f60458279513830070c6e4d47ff00e68
Francois Gergaud <fgergaud@anzen.com.mx>
2015-02-17 11:47:36 -0600

    implement Rest-API invocation for CEDE investment

--- SPA/app/scripts/controllers/investmentCedePrlvController.js
+++ SPA/app/scripts/controllers/investmentCedePrlvController.js
@@ -10,21 +10,6 @@ angular.module('spaApp').controller('InvestmentCedePrlvCtrl', ['$rootScope', '$s
 
     initialize();
 
-    /**
-     * set the investment type (CEDE or PRLV)
-     */
-    $scope.setInvestmentType = function(investmentCategory){
-        $scope.investmentCategory = investmentCategory;
-        //initialize the instruction investment array
-        //TODO: manage the labels with i18n
-        $scope.investmentInstructions = [{'code' : 1, 'label' : "Transferencia Cuenta Eje"}];
-        if($scope.investmentCategory === 'CEDE'){
-            $scope.investmentInstructions.push({'code' : 2, 'label' : "Reinversión de Capital con Pago de Interés"});
-        }else if($scope.investmentCategory === 'PRLV'){
-            $scope.investmentInstructions.push({'code' : 3, 'label' : "Reinversión de Capital e Intereses"});
-        }
-    }
-
     function initialize(){
         //Get investments products.
         productProvider.getProductsList().then(
@@ -36,21 +21,68 @@ angular.module('spaApp').controller('InvestmentCedePrlvCtrl', ['$rootScope', '$s
         $scope.ownAccounts = $rootScope.accounts;
         $scope.step = 1;
         $scope.investment = [];
-        
+        $scope.investmentResult = [];
+    }
+
+    /**
+     * set the investment type (CEDE or PRLV)
+     */
+    $scope.setInvestmentType = function(investmentCategory){
+        $scope.investmentCategory = investmentCategory;
+        //initialize the instruction investment array
+        //TODO: manage the labels with i18n
+        $scope.investmentInstructions = [{'investAgain' : false, 'label' : "Transferencia Cuenta Eje"}];
+        if($scope.investmentCategory === 'CEDE'){
+            $scope.investmentInstructions.push({'investAgain' : true, 'label' : "Reinversión de Capital con Pago de Interés"});
+        }else if($scope.investmentCategory === 'PRLV'){
+            $scope.investmentInstructions.push({'investAgain' : true, 'label' : "Reinversión de Capital e Intereses"});
+        }
     }
 
     /**
      * Function to navigate between steps.
 	 */
-	$scope.nextStep = function() {
+	$scope.goToConfirmation = function() {
         $scope.step++;
+        $scope.today = new Date().getTime();
 	 };
 
-     /**
+    /**
      * Function to navigate between steps.
      */
     $scope.cancel = function() {
         initialize();
      };
+
+     /**
+      * launch the investment operation
+      */
+     $scope.launchInvestment = function(){
+        var currentInvestment = $scope.investment;
+        var originAccountId = currentInvestment.originAccount._account_id;
+        var productId = currentInvestment.destinationProduct.product_id;
+        var amount = currentInvestment.amount;
+        var investAgain = currentInvestment.expirationInstruction.investAgain;
+        if($scope.investmentCategory === 'CEDE'){
+            transferProvider.investCEDE(originAccountId, productId, amount, investAgain).then(
+                function(data) {
+                    console.log(data);
+                    $scope.investmentResult = [];
+                    $scope.investmentResult.account_number = data.account_number;
+                    $scope.investmentResult.expiration_date = data.expiration_date;
+                    if(data.interest != null){
+                        $scope.investmentResult.interestInfo =[];
+                        $scope.investmentResult.interestInfo.operation_date = data.interest.operation_date;
+                        $scope.investmentResult.interestInfo.amount = data.interest.amount;
+                    }
+                    $scope.step++;
+                },
+                function(errorObject){
+                    var status = errorObject.status;
+                    var data = errorObject.data;
+                }
+            );
+        }
+     }
 
 }]);
