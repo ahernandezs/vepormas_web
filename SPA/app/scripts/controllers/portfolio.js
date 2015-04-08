76df1879a3778ade3ff6d750e840f8967d743f7b
Francois Gergaud <fgergaud@anzen.com.mx>
2015-04-08 17:24:06 -0500

    manage the case of all investment-ptoducts unavailable

--- SPA/app/scripts/controllers/portfolio.js
+++ SPA/app/scripts/controllers/portfolio.js
@@ -5,21 +5,34 @@
  */
 angular.module('spaApp').controller('PortfolioCtrl', ['$rootScope', '$scope', '$location', '$routeParams', 'accountsProvider', 'transferProvider', 'productProvider', function ($rootScope, $scope, $location, $routeParams, accountsProvider, transferProvider, productProvider) {
 
-    $scope.invType = 'VISTA';
-
      //Get investments products.
     productProvider.getProductsList().then(
       function(data) {
           $scope.investmentProducts = data.products;
-          $scope.vistaInvestmentAllowed = false;
           if(data.investment_vista_allowed){
           	$scope.vistaInvestmentAllowed = true;
+            $scope.invType = 'VISTA';
+          }else{
+            $scope.vistaInvestmentAllowed = false;
           }
           if(data.investment_prlv_allowed){
           	$scope.prlvInvestmentAllowed = true;
+            if($scope.invType == null){
+               $scope.invType = 'PRLV';
+            }
+          }else{
+            $scope.prlvInvestmentAllowed = false;
           }
           if(data.investment_cede_allowed){
           	$scope.cedeInvestmentAllowed = true;
+            if($scope.invType == null){
+               $scope.invType = 'CEDE';
+            }
+          }else{
+            $scope.cedeInvestmentAllowed = false;
+            if($scope.invType == null){
+              $scope.setServiceError('ninguno producto disponible');
+            }
           }
       },
       function(data) {
